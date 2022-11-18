07766abb2fab639ff6669ab24ef25c2a8e8a16aa
chinmay <chinmaymhatre12@gmail.com>
2022-11-18 16:40:43 +0530

    card ui update

--- components/GifCard.jsx
+++ components/GifCard.jsx
@@ -10,11 +10,11 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 const GifCard = ({post,isliked}) => {
     return (
-        <div className=" relative max-h-60 group rounded-lg ">
-        <div className="absolute bg-black group-hover:opacity-50 rounded-lg h-full w-full opacity-0 transition-all duration-100">
+        <div className=" relative max-h-60 group rounded-lg overflow-hidden ">
+        <div className="absolute bg-black z-10 group-hover:opacity-50 rounded-lg h-full w-full opacity-0 transition-all duration-100">
 
         </div>
-        <div className="absolute top-0 left-0 right-0 group-hover:scale-100  transition-all duration-100 scale-0 p-4">
+        <div className="absolute top-0 left-0 right-0 z-20 group-hover:opacity-100  transition-all duration-100 opacity-0 p-4">
             <div className="flex items-center">
             {isliked ? <FavoriteIcon className="text-red-500" /> : <FavoriteBorderIcon className="text-white" />}
             <p className="text-white pl-1">{post.likes.length}</p>
@@ -22,9 +22,8 @@ const GifCard = ({post,isliked}) => {
 
 
         </div>
-
-            <img src={post?.url} className = "max-h-60 rounded-lg  w-full object-cover" alt="card image" />
-            <div className="bg-none absolute scale-0 group-hover:scale-100 transition-all duration-100 bottom-0 text-white p-4 ">
+            <img src={post?.url} className = "max-h-60 rounded-lg group-hover:scale-110 transition-all duration-100 ease-in-out  w-full object-cover" alt="card image" />
+            <div className="bg-none z-20 absolute opacity-0 group-hover:opacity-100 transition-all duration-100 bottom-0 text-white p-4 ">
                 <h5 className="text-white text-2xl">
                     {post.title}
                 </h5>
