c45afb599a7c377ecebc763ea7ca8afd255365cf
chinmay <chinmaymhatre12@gmail.com>
2022-12-05 15:58:01 +0530

    tags progress

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,43 @@ import Link from "next/link";
 
 export default function Home() {
     const [posts, setPosts] = useState([]);
+    const [filteredPosts, setFilteredPosts] = useState([]);
     const [isliked, setIsliked] = useState(false);
     const [user, loading] = useAuthState(auth);
     const [showMore, setShowMore] = useState(false);
+    const [tags, setTags] = useState({});
+    const [selectedTag, setSelectedTag] = useState("All");
+
+    const filterByTag = (tag) => {
+        if (tag === "All") {
+            setSelectedTag(tag);
+            setFilteredPosts(posts);
+            return;
+        }
+        const filteredPosts = posts.filter((post) => post.tags.includes(tag));
+        setSelectedTag(tag);
+        setFilteredPosts(filteredPosts);
+    };
+
+    const getTags = () => {
+        const tags = {};
+        posts.forEach((post) => {
+            post.tags.forEach((tag) => {
+                if (tags[tag]) {
+                    tags[tag] += 1;
+                } else {
+                    tags[tag] = 1;
+                }
+            });
+        });
+        tags["All"] = posts.length;
+        const sortable = Object.fromEntries(
+            Object.entries(tags).sort(([, a], [, b]) => b - a)
+        );
+        setSelectedTag("All");
+
+        setTags(sortable);
+    };
 
     useEffect(() => {
         const getPosts = async () => {
@@ -25,6 +59,7 @@ export default function Home() {
                             posts.push({ id: doc.id, ...doc.data() });
                         });
                         setPosts(posts);
+
                         posts.forEach((post) => {
                             const result = post.likes.find(
                                 (id) => id === user?.uid
@@ -44,6 +79,11 @@ export default function Home() {
         getPosts();
     }, []);
 
+    useEffect(() => {
+        setFilteredPosts(posts);
+        getTags();
+    }, [posts]);
+
     return (
         <div className="font-montserrat">
             <Head>
@@ -60,52 +100,64 @@ export default function Home() {
                     <h1 className="text-4xl font-bold text-center text-white ">
                         All the Gifs
                     </h1>
-                    <div className="flex justify-between items-center">
-                        <h2 className="text-2xl font-medium text-white mt-5">
-                            Explore Moods
+                    <div className="flex justify-between items-center my-7">
+                        <h2 className="md:text-2xl text-xl font-medium text-white ">
+                            Explore Tags
                         </h2>
                         {showMore ? (
                             <button
                                 onClick={() => setShowMore(false)}
-                                className="text-white  font-bold text-lg bg-black align-self-center rounded-md px-4 py-2 mt-4"
+                                className=" font-medium text-lg text-gray-300  align-self-center rounded-md"
                             >
-                                Show Less
+                                Show Less -
                             </button>
                         ) : (
                             <button
                                 onClick={() => setShowMore(true)}
-                                className="text-white  font-bold text-lg bg-black align-self-center rounded-md px-4 py-2 mt-4"
+                                className="text-white text-lg align-self-center rounded-md  "
                             >
-                                Show More
+                                Show More +
                             </button>
                         )}
                     </div>
                     <div className=" flex justify-center items-center flex-col">
-                        <div className="grid grid-cols-5">
+                        <div className="grid md:grid-cols-3 grid-cols-2 lg:grid-cols-5 gap-5 ">
                             {showMore
-                                ? posts.map((post) => (
-                                      <GifCard
-                                          key={post.id}
-                                          post={post}
-                                          isliked={isliked}
-                                      />
+                                ? Object.keys(tags).map((tag) => (
+                                      <button
+                                          onClick={() => filterByTag(tag)}
+                                          className={`${
+                                              selectedTag == tag
+                                                  ? "text-black bg-white"
+                                                  : "text-white"
+                                          }  font-bold text-md border-2 border-white align-self-center transition-all duration-150 rounded-full px-4 py-2 mt-2`}
+                                      >
+                                          {tag}
+                                      </button>
                                   ))
-                                : posts
+                                : Object.keys(tags)
                                       .slice(0, 5)
-                                      .map((post) => (
-                                          <GifCard
-                                              key={post.id}
-                                              post={post}
-                                              isliked={isliked}
-                                          />
+                                      .map((tag) => (
+                                          <button
+                                              onClick={() => filterByTag(tag)}
+                                              className={`${
+                                                  selectedTag == tag
+                                                      ? "text-black bg-white"
+                                                      : "text-white"
+                                              }  font-bold text-md border-2 border-white transition-all duration-150 align-self-center rounded-lg px-4 py-2 mt-2`}
+                                          >
+                                              {tag}
+                                              <div className="text-gray-600">
+                                                  {tags[tag]} Posts
+                                              </div>
+                                          </button>
                                       ))}
                         </div>
-                        
                     </div>
                 </div>
                 <div className="lg:columns-4 md:columns-2 columns-1 gap-5 ">
-                    {posts &&
-                        posts.map((post) => (
+                    {filteredPosts &&
+                        filteredPosts.map((post) => (
                             <GifCard
                                 key={post.id}
                                 post={post}
